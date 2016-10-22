b698ec4501b2acbffb3b82950ced5717a12f38c9
Caden Jiang <jiangshen16@icloud.com>
2016-10-22 18:25:28 -0400

    campus chooser

---
+++ FoodApp/app/src/main/java/caden/foodapp/CampusChooser.java
@@ -0,0 +1,13 @@
+package caden.foodapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CampusChooser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_campus_chooser);
+    }
+}
