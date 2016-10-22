5eda6813c20905957d57033c047749efba05f039
skouriskaze <skouriskaze@gmail.com>
2016-10-22 19:19:48 -0400

    Added campus listview

--- FoodApp/app/src/main/java/caden/foodapp/CampusChooser.java
+++ FoodApp/app/src/main/java/caden/foodapp/CampusChooser.java
@@ -1,7 +1,17 @@
 package caden.foodapp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CampusChooser extends AppCompatActivity {
 
@@ -9,5 +19,37 @@ public class CampusChooser extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_campus_chooser);
+
+        List<Campus> campuses = new ArrayList<>();
+        campuses.add(new Campus("Harvard University", "Cambridge, MA 02138"));
+        campuses.add(new Campus("Massachusetts Institute of Technology", "77 Massachusetts Ave, Cambridge, MA 02139"));
+        CampusArrayAdapter adapter = new CampusArrayAdapter(this, campuses);
+
+        ListView lvCampuses = (ListView) findViewById(R.id.lvCampuses);
+        lvCampuses.setAdapter(adapter);
+    }
+
+    public class CampusArrayAdapter extends ArrayAdapter<Campus> {
+
+        public CampusArrayAdapter(Context context, List<Campus> campuses) {
+            super(context, -1, campuses);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            Campus curr = getItem(position);
+
+            if (convertView == null) {
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_campus, parent, false);
+            }
+
+            TextView tvName = (TextView) convertView.findViewById(R.id.tvCampusName);
+            TextView tvAddress = (TextView) convertView.findViewById(R.id.tvCampusAddress);
+
+            tvName.setText(curr.getName());
+            tvAddress.setText(curr.getAddress());
+
+            return convertView;
+        }
     }
 }
